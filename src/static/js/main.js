75a43654527301e34c8c62c9751bad738a2f1117
ghostrider070008 <ghostrider07008@yandex.ru>
2019-12-26 16:51:43 +0300

    Сделал прдудыщие и следубщие месяца

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -40,13 +40,100 @@ let d = new Date();
 let year = d.getFullYear();
 let month1 = d.getUTCMonth();
 let first_day = new Date();
-    d.setDate(d.getDate() + 8 ); 
+    d.setDate(d.getDate() + 1 );
 console.log(d.getDate());
 
-
-var app = new Vue({
+/* Вывод месяца */
+let appMonth = new Vue({
     el: '#app-month',
     data: {
       message: month
     }
   })
+/* Вывод года */
+ let appYear = new Vue({
+    el: '#app-year',
+    data: {
+        message: year
+    }
+});
+let appWeekday = new Vue({
+    el: '#app-weekday',
+    data:{
+        weekdays: [
+            {text: 'Пн'},
+            {text: 'Вт'},
+            {text: 'Ср'},
+            {text: 'Чт'},
+            {text: 'Пт'},
+            {text: 'Сб'},
+            {text: 'Вс'}
+
+        ]
+    }
+});
+/* Функция первого дня месяца */
+function receptionFirstDay(){
+    let firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    return firstDay;
+}
+/* Функция дня месяца */
+function receptionDay(i){
+    let Day = new Date(date.getFullYear(), date.getMonth(), i);
+    return Day;
+}
+/* Функция последнего дня месяца */
+function receptionLasttDay(){
+    let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    return lastDay;
+}
+let firstDay = receptionFirstDay();
+let lastDay = receptionLasttDay();
+console.log(firstDay.getDate());
+console.log(lastDay.getDate());
+
+let days = [];
+let day = firstDay.getDate();
+day = parseInt(day);
+console.log(day);
+for(let i = firstDay.getDate(); i <= lastDay.getDate();i++){
+    days[i] = receptionDay(i);
+}
+console.log(days);
+let differenceDay = 0;
+let mas = [];
+/* проверка на номер дня недели */
+if(firstDay.getDay() !== 1){
+    differenceDay = 7 - firstDay.getDay() - 1;
+    console.log(differenceDay);
+    mas = gettingPreviousMonth(firstDay, differenceDay );
+}
+//for(let i = 1; i <= differenceDay; i++){
+    console.log(mas);
+//}
+
+/* Функция поиска чисел предыдущего месяца */
+function gettingPreviousMonth(firstDay, differenceDay){
+    let mas = [];
+    firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    for(let i = 0; i < differenceDay; i++){
+        mas[i] = new Date(date.getFullYear(), date.getMonth(), firstDay.getDay() - i);
+    }
+    return mas;
+
+}
+
+/* Функция поиска чисел следующего месяца */
+function gettingNextMonth(lastDay, differenceDay){
+    let mas = [];
+    lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+    for(let i = 0; i <= differenceDay; i++){
+        mas[i] = new Date(date.getFullYear(), date.getMonth() + 1, 1 + i );
+    }
+    return mas;
+
+}
+
+differenceDay = 7 - lastDay.getDay() - 1;
+let next_month = gettingNextMonth(lastDay, differenceDay);
+console.log(next_month);
